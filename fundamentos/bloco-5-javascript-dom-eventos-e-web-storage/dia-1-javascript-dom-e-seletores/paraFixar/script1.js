553ed6eb8a708d2565a54f325e2ebdc1679f5e90
Lonardow <lonardow_fg@hotmail.com>
2021-09-02 17:51:29 -0300

    para fixar dom

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/paraFixar/script1.js
@@ -0,0 +1,13 @@
+document.getElementById("page-title").innerText = "Interstellar";
+
+document.getElementById("second-paragraph").innerText  = "alô gugu"
+
+document.getElementById("subtitle").innerText  = "Aô goiania"
+
+
+document.getElementsByClassName("paragrafo")[0].innerText = "repolho"
+
+document.getElementsByTagName("subtitle")[0].innerHTML
+
+
+
